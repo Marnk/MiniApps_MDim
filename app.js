e90c329424a6af05e8625faeb9144908517549bd
Young Yi <M_arnk@163.com>
2018-05-05 17:16:00 +0800

    2018050501_PC-gs

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         if (res.code) {
           //发起网络请求
           wx.request({
-            url: 'http://192.168.1.181',
+            url: 'https://api.marnk.com/api/wxlogo',
             data: {
               code: res.code
             },
             header: {
-              'content-type' : 'application/json', // 默认值
+              'content-type' : 'application/json', //默认值
               'x-requested-with' : 'xmlhttprequest'
             },
             success: (res) => {
@@ -33,7 +34,8 @@ App({
     // 获取用户信息
     wx.getSetting({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
+        console.log(res.authSetting['scope.userInfo'])
+        if (!res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
